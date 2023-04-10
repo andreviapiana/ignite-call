5c66662abb29687a057afa756d22389937c2afd3
André Viapiana <andreviapiana@gmail.com>
2023-04-10 16:00:45 -0300

    ✨feat: Cadastro de usuário - Persistindo/Criando usuário no Prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -9,6 +9,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
+import { api } from '../../lib/axios'
+
 const registerFormSchema = z.object({
   username: z
     .string()
@@ -43,8 +45,16 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
+
   return (
     <Container>
       <Header>
